328966311ca370641b5edb962ddf94a9af4b61ef
Optional <sahilr00000@gmail.com>
2023-05-15 17:29:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// $(document).ready(function(){
+//     $("#button").click(function(){alert("SS")});
+// })
+
+$(document).ready(function(){
+    $(".cross").click(function(){
+    $(".signuppage").hide()
+})
+})
+
+$(document).ready(function(){
+    $(".signupbutton").click(function(){
+        $(".signuppage").show()
+    })
+})
+
+
+$(document).ready(function(){
+    $(".crosstwo").click(function(){
+    $(".signinpage").hide()
+})
+})
+
+$(document).ready(function(){
+    $(".signin").click(function(){
+        $(".signinpage").show()
+    })
+})
+
+
+$(document).ready(function(){
+    $("#signuplogin").click(function(){
+        $(".signinpage").show(),
+        $(".signuppage").hide()
+    })
+})
+
+
+
